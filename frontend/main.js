9c4cf410bd92a82fd2120722a3dae1d945fcf617
lucasxpaire <lxpaire@inf.ufsm.br>
2026-09-07 23:33:04 -0300

    Altera criação dos blocos de lógica, matemática e loop

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,52 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    var workspace = Blockly.inject('blocklyDiv', {
-        toolbox: document.getElementById('toolbox'),
+    const toolbox = {
+        "kind": "categoryToolbox",
+        "contents": [
+            {
+                "kind": "category",
+                "name": "Lógica",
+                "colour": "%{BKY_LOGIC_HUE}",
+                "contents": [
+                    { "kind": "block", "type": "controls_if" },
+                    { "kind": "block", "type": "logic_compare" },
+                    { "kind": "block", "type": "logic_operation" }
+                ]
+            },
+            {
+                "kind": "category",
+                "name": "Loops",
+                "colour": "%{BKY_LOOPS_HUE}",
+                "contents": [
+                    {
+                        "kind": "block",
+                        "type": "controls_repeat_ext",
+                        "inputs": {
+                            "TIMES": {
+                                "shadow": {
+                                    "type": "math_number",
+                                    "fields": { "NUM": 10 }
+                                }
+                            }
+                        }
+                    },
+                    { "kind": "block", "type": "controls_whileUntil" }
+                ]
+            },
+            {
+                "kind": "category",
+                "name": "Matemática",
+                "colour": "%{BKY_MATH_HUE}",
+                "contents": [
+                    { "kind": "block", "type": "math_number" },
+                    { "kind": "block", "type": "math_arithmetic" }
+                ]
+            }
+        ]
+    };
+
+    var workspace = Blockly.inject('workspace', {
+        toolbox: toolbox,
         scrollbars: true,
         trashcan: true
     });
