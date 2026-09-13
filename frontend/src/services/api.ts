4076aae99c7a759b4d4c5d11fe05b157f91a2ee2
lucasxpaire <lxpaire@inf.ufsm.br>
2026-09-13 19:28:04 -0300

    Adiciona variaveis de ambiente

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Estatistica } from '../types/Estatistica';
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api/estatisticas"
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api/estatisticas"
 });
 
 export const api = {
