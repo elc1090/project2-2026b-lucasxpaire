170eec2d78b6df8a84ef0a8280b7f0a7f271d9e3
lucasxpaire <lxpaire@inf.ufsm.br>
2026-09-13 15:26:21 -0300

    Arruma bugs de estado do componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import BlocklyComponent from './components/Blockly';
 
 function App() {
+  const [codigoJS, setCodigoJS] = useState("");
+  const [blocosJSON, setBlocosJSON] = useState({});
+
   return (
     <div className="app-container">
       <header>
         <h1>Desafio: Lógica de Programação</h1>
       </header>
       <main>
-        {/* O componente do Blockly entrará aqui */}
-        <p>A lousa mágica ficará aqui!</p>
+        <BlocklyComponent onChange={(codigo, json) => { setCodigoJS(codigo); setBlocosJSON(json) }} />
       </main>
     </div>
   )
--- frontend/src/components/Blockly.tsx
+++ frontend/src/components/Blockly.tsx
@@ -6,71 +6,77 @@ interface BlocklyProps {
     onChange: (codigoJS: string, blocosJSON: any) => void;
 }
 
-export default function BlocklyComponent({ onChange }: BlocklyProps) {
-    const config = {
-        "kind": "categoryToolbox",
-        "contents": [
-            {
-                "kind": "category",
-                "name": "Lógica",
-                "colour": "%{BKY_LOGIC_HUE}",
-                "contents": [
-                    { "kind": "block", "type": "controls_if" },
-                    { "kind": "block", "type": "logic_compare" },
-                    { "kind": "block", "type": "logic_operation" }
-                ]
-            },
-            {
-                "kind": "category",
-                "name": "Loops",
-                "colour": "%{BKY_LOOPS_HUE}",
-                "contents": [
-                    {
-                        "kind": "block",
-                        "type": "controls_repeat_ext",
-                        "inputs": {
-                            "TIMES": {
-                                "shadow": {
-                                    "type": "math_number",
-                                    "fields": { "NUM": 10 }
-                                }
+const toolboxConfig = {
+    "kind": "categoryToolbox",
+    "contents": [
+        {
+            "kind": "category",
+            "name": "Lógica",
+            "colour": "%{BKY_LOGIC_HUE}",
+            "contents": [
+                { "kind": "block", "type": "controls_if" },
+                { "kind": "block", "type": "logic_compare" },
+                { "kind": "block", "type": "logic_operation" }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Loops",
+            "colour": "%{BKY_LOOPS_HUE}",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "controls_repeat_ext",
+                    "inputs": {
+                        "TIMES": {
+                            "shadow": {
+                                "type": "math_number",
+                                "fields": { "NUM": 10 }
                             }
                         }
-                    },
-                    { "kind": "block", "type": "controls_whileUntil" }
-                ]
-            },
-            {
-                "kind": "category",
-                "name": "Matemática",
-                "colour": "%{BKY_MATH_HUE}",
-                "contents": [
-                    { "kind": "block", "type": "math_number" },
-                    { "kind": "block", "type": "math_arithmetic" }
-                ]
-            },
-            {
-                "kind": "category",
-                "name": "Texto",
-                "colour": "%{BKY_TEXTS_HUE}",
-                "contents": [
-                    { "kind": "block", "type": "text" },
-                    { "kind": "block", "type": "text_print" }
-                ]
-            },
-            {
-                "kind": "category",
-                "name": "Variáveis",
-                "custom": "VARIABLE",
-                "colour": "%{BKY_VARIABLES_HUE}"
-            }
-        ]
-    };
+                    }
+                },
+                { "kind": "block", "type": "controls_whileUntil" }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Matemática",
+            "colour": "%{BKY_MATH_HUE}",
+            "contents": [
+                { "kind": "block", "type": "math_number" },
+                { "kind": "block", "type": "math_arithmetic" }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Texto",
+            "colour": "%{BKY_TEXTS_HUE}",
+            "contents": [
+                { "kind": "block", "type": "text" },
+                { "kind": "block", "type": "text_print" }
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Variáveis",
+            "custom": "VARIABLE",
+            "colour": "%{BKY_VARIABLES_HUE}"
+        }
+    ]
+};
 
+const workspaceConfig = {
+    scrollbars: true,
+    trashcan: true
+};
+
+export default function BlocklyComponent({ onChange }: BlocklyProps) {
     return (
         <BlocklyWorkspace
             className="workspace-container"
-            toolboxConfiguration={config} workspaceConfiguration={{ scrollbars: true, trashcan: true }}
+            toolboxConfiguration={toolboxConfig}
+            workspaceConfiguration={workspaceConfig}
             onWorkspaceChange={(workspace) => {
                 const codigoConvertido = javascriptGenerator.workspaceToCode(workspace)
                 const jsonConvertido = Blockly.serialization.workspaces.save(workspace);
