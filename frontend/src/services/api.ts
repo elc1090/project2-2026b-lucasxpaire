ff1113caf2841c0be983dc753dddc9420ff8a15c
lucasxpaire <lxpaire@inf.ufsm.br>
2026-09-13 19:01:50 -0300

    Adiciona servico de api e dependencia do axios

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import type { Estatistica } from '../types/Estatistica';
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:8080/api/estatisticas"
+});
+
+export const api = {
+  salvar: async (estatistica: Estatistica): Promise<void> => {
+    await apiClient.post("", estatistica);
+  },
+  listar: async (): Promise<Estatistica[]> => {
+    const resposta = await apiClient.get<Estatistica[]>("");
+    return resposta.data;
+  }
+};
